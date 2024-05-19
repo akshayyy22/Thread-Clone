5e1affc49b87a15fa801cf978a5e4bd268b500c1
akshayyy22 <akshaypersonalstudentid@gmail.com>
2024-05-19 10:20:44 +0530

    Readme Update

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -4,6 +4,7 @@ export default function Page() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
       <SignIn />
+      <div className="absolute bottom-0 right-0 m-6 px-4 py-2 bg-slate-800 rounded-full text-slate-400 text-sm font-medium">Created by Akshay Esackimuthu</div>
     </div>
   );
 }
